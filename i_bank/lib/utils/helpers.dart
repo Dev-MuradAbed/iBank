e078be2d2682f0af2a99a24a3f1ad15c79054c94
Dev-MuradAbed <dev.muradabed@gmail.com>
2024-08-06 23:23:08 +0300

    create desgin for response api in console

--- i_bank/lib/utils/helpers.dart
+++ i_bank/lib/utils/helpers.dart
@@ -1,7 +1,26 @@
 import 'dart:developer';
+import 'dart:developer' as developer;
+
+import 'index.dart';
 
 void consoleLog(dynamic value, {dynamic key = 'value'}) {
   log('📔:\x1B[32m ******** Log $key **********:📔');
   log('\x1B[35m $key :\x1B[37m $value');
   log('📓:\x1B[32m  *************** END **************:📓');
+}
+
+/// print request details
+void showRequestDetails({
+  dynamic method,
+  dynamic path,
+  dynamic body,
+}) {
+  if (showLog) {
+    developer.log('📘\x1B[36m ******** Request Details **********📘');
+    developer.log('\x1B[35m Full URL: \x1B[37m $baseUrl$path');
+    developer.log('\x1B[35m Method: \x1B[37m $method');
+    developer.log('\x1B[35m Path: \x1B[37m $path');
+    developer.log('\x1B[35m Body: \x1B[37m $body');
+    developer.log('\x1B[36m  *************** END **************');
+  }
 }
