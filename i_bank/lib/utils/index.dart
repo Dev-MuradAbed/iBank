78c7d9e88d4727e5bdf0b11eecb50941ec30b78a
Dev-MuradAbed <dev.muradabed@gmail.com>
2024-08-06 23:57:39 +0300

    create index files

--- i_bank/lib/utils/index.dart
+++ i_bank/lib/utils/index.dart
@@ -1,2 +1,7 @@
+export 'package:get/get.dart';
+
+export './constant.dart';
+export './helpers.dart';
+export './local_storage.dart';
 export '../screens/my_app/indext.dart';
 export '../styles/index.dart';
